de0abc239b549a17c6c78c7562ad00a1706b0464
Abdarahmane <abda.iba.demba1@gmail.com>
2024-10-06 19:13:46 +0000

    update the validator

--- src/validators/Validator.js
+++ src/validators/Validator.js
@@ -7,37 +7,38 @@ const validateCreateRecipe = () => {
       .isEmpty()
       .withMessage("Le titre ne peut pas être vide!")
       .bail()
-      .isLength({ min: 5,max :100 })
-      .withMessage("Minimum 100 caractères requis!"),
+      .isLength({ min: 5, max: 100 }) // Assurez-vous que min et max soient corrects
+      .withMessage("Le titre doit comporter entre 5 et 100 caractères!"),
     check("ingredients")
-      .not()
-      .isEmpty()
-      .withMessage("Les ingrédients ne peuvent pas être vides!"),
-    check("type").not().isEmpty().withMessage("Le type ne peut pas être vide!"),
-  ];
-};
-
-
-const validateUpdateRecipe = () => {
-  return [
-    param("id").not().isEmpty().withMessage("L'ID est requis!"),
-    check("titre")
-      .optional() 
-      .isLength({ min: 5 ,max:100 })
-      .withMessage("Minimum 6 caractères requis pour le titre!"),
-    check("ingredients")
-      .optional() 
       .not()
       .isEmpty()
       .withMessage("Les ingrédients ne peuvent pas être vides!"),
     check("type")
-      .optional() 
       .not()
       .isEmpty()
       .withMessage("Le type ne peut pas être vide!"),
   ];
 };
 
+const validateUpdateRecipe = () => {
+  return [
+    param("id").not().isEmpty().withMessage("L'ID est requis!"),
+    check("titre")
+      .optional()
+      .isLength({ min: 5, max: 100 })
+      .withMessage("Le titre doit comporter entre 5 et 100 caractères!"),
+    check("ingredients")
+      .optional()
+      .not()
+      .isEmpty()
+      .withMessage("Les ingrédients ne peuvent pas être vides!"),
+    check("type")
+      .optional()
+      .not()
+      .isEmpty()
+      .withMessage("Le type ne peut pas être vide!"),
+  ];
+};
 
 const validateDeleteRecipe = () => {
   return [
@@ -48,16 +49,13 @@ const validateDeleteRecipe = () => {
   ];
 };
 
-
 const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
-
   next();
 };
-
 
 export {
   validateCreateRecipe,
