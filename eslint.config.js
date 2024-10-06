3bcc712498c35342483c763dddd741720d7052a0
Abdarahmane <abda.iba.demba1@gmail.com>
2024-10-06 19:49:34 +0000

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,21 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,   
+        describe: "readonly",  
+        it: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+      },
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "off",  
+    },
+  },
 ];
